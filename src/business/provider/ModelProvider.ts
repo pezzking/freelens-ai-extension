a41e517419b0668388d27ee2bafd2ce99c204f0f
leo-capvano <60476114+leo-capvano@users.noreply.github.com>
2025-05-27 20:34:29 +0200

    added support for FREELENS_OLLAMA_HOST and FREELENS_OLLAMA_PORT env variable

--- src/business/provider/ModelProvider.ts
+++ src/business/provider/ModelProvider.ts
@@ -2,7 +2,8 @@ import { ChatOpenAI } from "@langchain/openai";
 import { ChatOllama } from "@langchain/ollama";
 import { AIModels } from "./AIModels";
 
-const FREELENS_OLLAMA_BASE_URL = process.env.FREELENS_OLLAMA_BASE_URL || "http://127.0.0.1:9898";
+const FREELENS_OLLAMA_HOST = process.env.FREELENS_OLLAMA_HOST || "http://127.0.0.1";
+const FREELENS_OLLAMA_PORT = process.env.FREELENS_OLLAMA_PORT || "9898";
 
 type ModelStrategy = {
   modelName: AIModels;
@@ -24,12 +25,12 @@ export const useModelProvider = () => {
       case "llama3.2:1b":
       case "mistral:7b":
         let headers = new Headers();
-        headers.set("Origin", "http://127.0.0.1");
+        headers.set("Origin", FREELENS_OLLAMA_HOST);
         return new ChatOllama({
           model: modelName,
           temperature: 0,
           headers: headers,
-          baseUrl: FREELENS_OLLAMA_BASE_URL,
+          baseUrl: `${FREELENS_OLLAMA_HOST}:${FREELENS_OLLAMA_PORT}`,
         });
       default:
         throw new Error(`Unsupported model: ${modelName}`);
